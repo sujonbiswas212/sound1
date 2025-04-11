36dcce0f269473d922eaa5f4c199a9957185106e
sujonbiswas212 <wwwsujonbiswas212@gmail.com>
2025-04-12 00:21:43 +0600

    fi

--- app.js
+++ app.js
@@ -1,8 +1,8 @@
-var recognition = new webkitSpeechRecognition(); 
+var recognition = new webkitSpeechRecognition();
 recognition.onresult = function(event){
      var text = event.results[0][0].transcript;
      var output = document.getElementById('output').innerHTML = text;
-     console.log(text);
+
      read(text)
 }
 
@@ -10,18 +10,18 @@ function read(text){
      var speech = new SpeechSynthesisUtterance();
      speech.text = text;
      if(text == 'Hello'){
-          speech.text = 'bye'
-
-
+          speech.text ='bye';
      }
-     else if(text == "What is you name"){
-          speech.text = "my name is sujon biswas"
+     else if(text=='What is your name'){
+          speech.text = 'my name is sujon biswas';
      }
-
+     else if(text == 'my favorite player'){
+          speech.text = 'leneo messi';
+     }
      else{
-          speech.text="please try agin";
+          speech.text ="please try agin";
      }
 
      document.getElementById('res').innerHTML=speech.text;
-     window.speechSynthesis.speak();
+     window.speechSynthesis.speak(speech);
 }
